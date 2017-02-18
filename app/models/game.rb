7cc4e91a3b57d91f84d7d4e8f0c13ecfb500186b
Katie O'Neill <katiemarieoneill@gmail.com>
2017-02-17 17:35:16 -0800

    Create method to display word as blank spaces or the guessed letter

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,28 @@
 require 'net/http'
 
 class Game < ApplicationRecord
-  def word
+  def initialize
     uri = URI("http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words")
-    Net::HTTP.get(uri).split("\n").sample
+    @word = Net::HTTP.get(uri).split("\n").sample
+    @guessed_letters = []
+    word_display
   end
+
+
+  def word_display
+    display_word = ""
+    @word.each_char do |l|
+      if @guessed_letters.include? l
+        display_word << l
+      else
+        display_word << "-"
+      end
+
+    end
+
+    return display_word
+  end
+
+
 end
+
